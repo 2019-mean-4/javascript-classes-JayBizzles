fec28580edbbbe79af77b049a3116210f40ce187
JayB <DooDooRapist@gmail.com>
2019-07-22 13:09:48 -0700

    4 of the properties added and made isAlive true

--- person-challenge/person.ts
+++ person-challenge/person.ts
@@ -2,8 +2,14 @@ export class person{
     firstName: string;
     lastName: string;
     age: number;
-    isAlive: true;
+    isAlive: boolean;
+        constructor(firstName: string, lastName:string, age: number, isAlive: boolean){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.isAlive = true;
+        }
 }
 
-let humanoidA = new person();
+let humanoidA = new person("fname", "lname", 12, true);
 
